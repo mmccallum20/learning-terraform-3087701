e1bdc94866ef5df21c0a3e8ccfba66e1f250eeba
mmccallum20 <70548091+mmccallum20@users.noreply.github.com>
2025-04-22 18:04:14 +0100

    Changed module to var

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "target_groups" {
   description = "Map of target groups created and their attributes"
-  value       = module.aws_lb.target_groups
+  value       = var.aws_lb.target_groups
 }
