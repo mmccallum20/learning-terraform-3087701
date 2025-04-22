ff75c3c137c99b977bf4a20a2b0381f2825dc741
mmccallum20 <70548091+mmccallum20@users.noreply.github.com>
2025-04-22 18:02:40 +0100

    added aws_lb to variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,22 @@ variable "aws_security_group" {
   type = list(string)
   default     = ["default"]
 }
+
+variable "aws_lb" {
+  description = "Configuration for the AWS Application Load Balancer"
+  type = object({
+    name               = string
+    internal           = bool
+    security_groups    = list(string)
+    subnets            = list(string)
+    enable_deletion_protection = bool
+  })
+  default = {
+    name               = "my-load-balancer"
+    internal           = false
+    security_groups    = ["sg-0123456789abcdef0"]
+    subnets            = ["subnet-0123456789abcdef0", "subnet-0123456789abcdef1"]
+    enable_deletion_protection = false
+  }
+}
+
