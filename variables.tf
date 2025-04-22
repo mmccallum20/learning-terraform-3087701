7f047f114e3df4fa9e47738156910269b2130cd5
mmccallum20 <70548091+mmccallum20@users.noreply.github.com>
2025-04-22 18:00:15 +0100

    Add aws security group to variables

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "aws_security_group" {
+  type = list(string)
+  default     = ["default"]
+}
