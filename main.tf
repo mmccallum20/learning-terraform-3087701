cff303d168cc9d513bf59ea6e340660f765a45f5
mmccallum20 <70548091+mmccallum20@users.noreply.github.com>
2025-04-23 10:53:00 +0100

    Updated ln 72 name with dash not underscore

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ module "blog_alb" {
   source = "terraform-aws-modules/alb/aws"
   version = "~> 6.0"
 
-  name = "blog_alb"
+  name = "blog-alb"
   load_balancer_type = "application"
 
   vpc_id = module.blog_vpc.vpc_id
